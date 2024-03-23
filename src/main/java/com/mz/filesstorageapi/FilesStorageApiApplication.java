51b2623ee62f94a8043f38ab7b63ca877d313a07
unknown <iyonissio01@gmail.com>
2024-03-23 14:20:05 +0200

    complete file upload mechanism

---
+++ src/main/java/com/mz/filesstorageapi/FilesStorageApiApplication.java
@@ -0,0 +1,13 @@
+package com.mz.filesstorageapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilesStorageApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilesStorageApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mz/filesstorageapi/FilesStorageApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mz.filesstorageapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilesStorageApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
